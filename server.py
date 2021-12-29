1f17cb07acf3d0a03424fa9aeae8d3fdae3365a3
avboer <531781663@qq.com>
2021-12-29 11:54:51 +0800

    add server.py

--- server.py
+++ server.py
@@ -1,7 +1,29 @@
-#!/bin/bash
-git clone https://github.com/avboer/pierced.git
-cd pierced
-cd mac_64
-chmod 777 ./ding
-./ding -config=./ding.cfg -subdomain=$1 8080 &
-python3 ../../server.py $2
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import sys
+
+data = sys.argv[1]
+host = ('localhost', 8080)
+
+
+class Resquest(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+    def do_POST(self):
+        datas = self.rfile.read(int(self.headers['content-length']))
+
+        print('headers', self.headers)
+        print("do post:", self.path, self.client_address, datas)
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Resquest)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
